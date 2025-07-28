4b6e6145f881625ffd7dc2e4fcb09b0b8c165b2e
karllabatistaa <kblb@icomp.ufam.edu.br>
2025-07-28 09:23:06 -0400

    change input to NotificationRequest

--- notification-request-service/app/use_cases/publish_notification.py
+++ notification-request-service/app/use_cases/publish_notification.py
@@ -1,11 +1,12 @@
 from app.domain.messaging.event_bus import EventBus
 from app.domain.exceptions.notification_publish_error import NotificationPublishError
+from app.domain.entities.notification_request import NotificationRequest
 class PublishNotificationUseCase:
 
     def __init__(self,event_bus:EventBus):
         self.event_bus = event_bus
 
-    def execute(self,event:dict) -> None:
+    def execute(self,event:NotificationRequest) -> None:
 
         try:
             self.event_bus.publish(event)
