fddb33f4bd5fb73e4b812e86a8e3b837fee91d35
karllabatistaa <kblb@icomp.ufam.edu.br>
2025-07-28 00:27:55 -0400

    improve class

--- notification-request-service/app/domain/entities/notification_request.py
+++ notification-request-service/app/domain/entities/notification_request.py
@@ -1,7 +1,10 @@
 class NotificationRequest:
-
+    channels = {"email","sms","push"}
     
-    def __init__(self,user_id,message,channel):
+    def __init__(self,user_id:str,message:str,channel:str):
+
+        if not self.validate(user_id,message,channel):
+            raise ValueError("Invalid data to Notification Request")
         self.user_id = user_id
         self.message = message
         self.channel = channel
@@ -13,3 +16,12 @@ class NotificationRequest:
             "message": self.message,
             "channel":self.channel
         }
+
+    @staticmethod
+    def validate(user_id:str,message:str,channel:str) ->bool:
+      
+        if not user_id or not message:
+            return False
+        if channel not in NotificationRequest.channels:
+            return False
+        return True
