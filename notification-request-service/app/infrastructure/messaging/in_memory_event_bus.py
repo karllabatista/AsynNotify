e52a9627583aa98139e90075a69b90f9f8c12902
karllabatistaa <kblb@icomp.ufam.edu.br>
2025-07-28 09:41:57 -0400

    add retry to queue

--- notification-request-service/app/infrastructure/messaging/in_memory_event_bus.py
+++ notification-request-service/app/infrastructure/messaging/in_memory_event_bus.py
@@ -1,7 +1,8 @@
 from app.domain.messaging.event_bus import EventBus
-from queue import Queue
+from queue import Queue,Full
 from app.domain.entities.notification_request import NotificationRequest
 import logging
+import time
 
 logging.basicConfig(level=logging.info)
 logger = logging.getLogger(__name__)
@@ -13,15 +14,23 @@ class InMemoryEventBus(EventBus):
 
     def publish(self,event:NotificationRequest)-> None:
         
-        try:
-            event_to_queue ={
-                "type": "NotificationRequest",
-                "data": event.to_dict()
-            }
+        success = False
+        for attempt in range(1,4):
+            try:
+                event_to_queue ={
+                    "type": "NotificationRequest",
+                    "data": event.to_dict()
+                }
 
-            self.queue.put(event_to_queue)
-            logger.info(f"Published event to queue: {event_to_queue}")
+                self.queue.put_nowait(event_to_queue)
+                logger.info(f"[Attempt {attempt}]:Published event to queue: {event_to_queue}")
+                success = True
+                break
 
-        except Exception as error:
-            logger.exception(f"Failed to publish event to queue:{error}")
-            raise
+            except Full :
+               logger.warning(f"[Attempt {attempt}] Queue full. Retrying...")
+               time.sleep(0.1 * attempt)
+        
+        if not success:
+            logger.error("All retries failed")
+            
