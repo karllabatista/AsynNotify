dff90bafb2fdb6a7d51c829bd0a911745fb2dcea
karllabatista <kblb@icomp.ufam.edu.br>
2025-07-31 16:11:54 -0400

    add loggint to use case

--- notification-publish-service/app/use_cases/publish_notification.py
+++ notification-publish-service/app/use_cases/publish_notification.py
@@ -1,13 +1,22 @@
 from app.domain.messaging.event_bus import EventBus
 from app.domain.exceptions.notification_publish_error import NotificationPublishError
 from app.domain.entities.notification_request import NotificationRequest
+import logging
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
 class PublishNotificationUseCase:
 
     def __init__(self,event_bus:EventBus):
         self.event_bus = event_bus
 
-    def execute(self,event:NotificationRequest) -> None:
+    def execute(self,notification:NotificationRequest) -> None:
+        logger.info(f"Trying to publish notification ...")
 
-        success = self.event_bus.publish(event)
+        success = self.event_bus.publish(notification.to_dict())
         if not success:
+            logger.error(f" Faile dto publish notification")
             raise NotificationPublishError("Failed to publish event")
+        
+        logger.info("Notication published with successful.")
