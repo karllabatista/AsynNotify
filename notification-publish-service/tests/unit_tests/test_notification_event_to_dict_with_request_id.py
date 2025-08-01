6ee394ead5ff35181a1567610746ad51a31bb167
karllabatista <kblb@icomp.ufam.edu.br>
2025-07-31 23:35:34 -0400

    add test of notification event: case dict and request_id

---
+++ notification-publish-service/tests/unit_tests/test_notification_event_to_dict_with_request_id.py
@@ -0,0 +1,21 @@
+from domain.events.notification_event import NotificationEvent
+from unittest.mock import Mock
+def test_notification_event_to_dict_with_request_id():
+    
+    request_id_mock = "request-123455"
+    notification_request_mock = Mock()
+    notification_request_mock.to_dict.return_value ={
+        "user_id" :"karlla12345",
+        "message": "recebemos seu pedido",
+        "channel":"email"
+    }
+
+
+    notification_event = NotificationEvent(notification_request_mock,request_id_mock)
+    result_data = notification_event.to_dict()
+
+    assert result_data["event_type"] == "NotificationRequested"
+    assert result_data["data"]["user_id"] == "karlla12345"
+    assert result_data["data"]["message"] == "recebemos seu pedido"
+    assert result_data["data"]["channel"] == "email"
+    assert result_data["metadata"]["request_id"] == request_id_mock
