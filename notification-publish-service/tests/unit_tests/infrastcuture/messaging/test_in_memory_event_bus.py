04518128c3491ef8cca276c0b9c157a214b0a0af
karllabatista <kblb@icomp.ufam.edu.br>
2025-08-01 17:15:02 -0400

    add test   test_publish_event_raises_queue_is_full

--- notification-publish-service/tests/unit_tests/infrastcuture/messaging/test_in_memory_event_bus.py
+++ notification-publish-service/tests/unit_tests/infrastcuture/messaging/test_in_memory_event_bus.py
@@ -1,6 +1,9 @@
 from unittest.mock import Mock
 from app.infrastructure.messaging.in_memory_event_bus import InMemoryQueueEventBus
 from app.domain.entities.notification_request import NotificationRequest
+from app.domain.exceptions.notification_publish_error import NotificationPublishError
+import pytest
+
 def test_publish_event():
     ## arrange
     
@@ -25,3 +28,25 @@ def test_publish_event():
     assert event_in_queue["data"]["channel"] == "email"
     assert event_in_queue["metadata"]["request_id"] == request_id_mock
     assert "timestamp" in event_in_queue["metadata"]
+
+def test_publish_event_raises_queue_is_full():
+    
+    # arrange
+
+    immemory = InMemoryQueueEventBus(maxsize=1)
+    notification_request_1= NotificationRequest(user_id="karlla.batista",
+                                              message="pagamento confirmado",
+                                              channel="email")
+    notification_request_2= NotificationRequest(user_id="karlla.batista",
+                                              message="pagamento confirmado",
+                                              channel="email")
+    request_id_mock = "abc1234-00"
+    
+    immemory.publish(notification_request_1,request_id_mock)
+
+    # Act + Assert: publicar mais um deve levantar exceção
+    
+    with pytest.raises(NotificationPublishError) as excinfo :
+        immemory.publish(notification_request_2,request_id_mock)
+    
+    assert "Queue is full" in str(excinfo.value)
