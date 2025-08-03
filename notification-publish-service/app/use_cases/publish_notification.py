473c19cc1bcafc53cc4517b12219a62ab3b38cab
karllabatista <kblb@icomp.ufam.edu.br>
2025-08-03 00:43:55 -0400

    fix wrong paramenter

--- notification-publish-service/app/use_cases/publish_notification.py
+++ notification-publish-service/app/use_cases/publish_notification.py
@@ -1,6 +1,6 @@
-from app.domain.ports.event_bus import EventBus
-from app.domain.exceptions.notification_publish_error import NotificationPublishError
-from app.domain.entities.notification_request import NotificationRequest
+from domain.ports.event_bus import EventBus
+from domain.exceptions.notification_publish_error import NotificationPublishError
+from domain.entities.notification_request import NotificationRequest
 import logging
 
 logging.basicConfig(level=logging.INFO)
@@ -14,9 +14,9 @@ class PublishNotificationUseCase:
     def execute(self,notification:NotificationRequest) -> None:
         logger.info(f"Trying to publish notification ...")
 
-        success = self.event_bus.publish(notification.to_dict())
+        success = self.event_bus.publish(notification)
         if not success:
-            logger.error(f" Faile dto publish notification")
+            logger.error(f" Failes dto publish notification")
             raise NotificationPublishError("Failed to publish event")
         
         logger.info("Notication published with successful.")
