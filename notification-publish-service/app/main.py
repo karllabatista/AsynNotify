8fb716b68fe9cf29d70d1351a3d1bb863bf7b81f
karllabatista <kblb@icomp.ufam.edu.br>
2025-08-03 01:06:18 -0400

    improve endpoint with create_notification_use_case()

--- notification-publish-service/app/main.py
+++ notification-publish-service/app/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException,Depends
 from starlette import status
 from interface.schemas.notification_input import NotificationInput
 from interface.schemas.notification_response import NotificationResponse
@@ -16,19 +16,28 @@ app = FastAPI()
 def hello_world():
 
     return {"message":"bosta de vida"}
+def create_notification_use_case():
+    event_bus = InMemoryQueueEventBus()
+    publish_notification_use_case = PublishNotificationUseCase(event_bus)
+    return publish_notification_use_case
 
 @app.post("/notifications",
           status_code=status.HTTP_200_OK,
           responses={  
             400: {"model": ErrorResponse, "description": "Validation Error"},
             500: {"model": ErrorResponse, "description": "Internal Error"},})
-def send_notification(notification_input: NotificationInput):
-  
-  event_bus = InMemoryQueueEventBus()
-  notification_req = NotificationRequest(user_id=notification_input.user_id,
-                                    channel=notification_input.channel,
-                                    message=notification_input.message)
-  
-  publish_notification_use_case = PublishNotificationUseCase(event_bus)
-  publish_notification_use_case.execute(notification_req)
-  return NotificationResponse(message="Notification sent successfully")
+def send_notification(notification_input: NotificationInput,
+                      notification_use_case:PublishNotificationUseCase = Depends(create_notification_use_case)):
+    try:
+        logger.info("Received request ..")
+        
+        notification_req = NotificationRequest(user_id=notification_input.user_id,
+                                          channel=notification_input.channel,
+                                          message=notification_input.message)
+        
+        notification_use_case.execute(notification_req)
+        return NotificationResponse(message="Notification sent successfully")
+    
+    except Exception as error:
+        logger.exception(f"Failed to publish notifdication:{error}")
+        raise HTTPException(status_code=500,detail="Internal error: Failed to publish notifdication")
