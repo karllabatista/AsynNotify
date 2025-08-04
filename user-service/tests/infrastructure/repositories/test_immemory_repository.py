1be6bfa7f9dd5f61e13008263e6fa7bf443af926
karllabatista <kblb@icomp.ufam.edu.br>
2025-08-04 15:57:55 -0400

    add test_immemorys_repository

---
+++ user-service/tests/infrastructure/repositories/test_immemory_repository.py
@@ -0,0 +1,28 @@
+from unittest.mock import patch
+from app.infrastructure.in_memory_repository import InMemoryRepository
+from app.domain.entities.contact_info import ContactInfo
+def test_get_user_by_id():
+
+    mock_user_db = {
+      
+        "mockeruser":{ 
+                "contact_info":
+                {
+                    "email":"mockeruser@gmail.com",
+                    "sms":"+55000000000",
+                    "prefered_channel":"email"
+                }
+        
+        }
+
+    }
+    with patch.object(InMemoryRepository,"USER_DB",mock_user_db):
+        inmemory = InMemoryRepository()
+        contact = inmemory.get_user_by_id("mockeruser")
+
+        assert isinstance(contact,ContactInfo)
+        assert contact.email == "mockeruser@gmail.com"
+        assert contact.sms == "+55000000000"
+        assert contact.prefered_channel == "email"
+    
+
