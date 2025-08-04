b9c4134e1d2e2f4246a162071fbfb1ab7cb12628
karllabatista <kblb@icomp.ufam.edu.br>
2025-08-03 23:35:40 -0400

    add function typing

--- notification-publish-service/app/infrastructure/redis_client.py
+++ notification-publish-service/app/infrastructure/redis_client.py
@@ -1,8 +1,8 @@
-import redis
+from redis import Redis
 from config.env import get_redis_host, get_redis_port
 
-def get_redis_connection():
-    return redis.Redis(
+def get_redis_connection()-> Redis:
+    return Redis(
         host=get_redis_host(),
         port=get_redis_port(),
         db=0,
