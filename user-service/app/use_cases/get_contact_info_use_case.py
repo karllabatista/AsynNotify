bab674f55610fa4199ccce7805e72eec88734dc1
karllabatista <kblb@icomp.ufam.edu.br>
2025-08-04 22:31:46 -0400

    change return of function

--- user-service/app/use_cases/get_contact_info_use_case.py
+++ user-service/app/use_cases/get_contact_info_use_case.py
@@ -1,9 +1,10 @@
 from app.domain.repositories.user_repository import UserRepository
+from app.domain.entities.contact_info import ContactInfo
 class GetContactInfoUseCase:
     
     def __init__(self,repository:UserRepository):
         self.repository = repository
 
-    def execute(self,user_id:str) -> bool:
-        self.repository.get_user_by_id(user_id)
+    def execute(self,user_id:str) -> ContactInfo:
+        return self.repository.get_user_by_id(user_id)
         
