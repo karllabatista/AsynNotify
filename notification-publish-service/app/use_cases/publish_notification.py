bb6135a4641bb2dee53307cf6325f804b60af07a
karllabatista <kblb@icomp.ufam.edu.br>
2025-08-05 14:33:44 -0400

    refactor and improve class

--- notification-publish-service/app/use_cases/publish_notification.py
+++ notification-publish-service/app/use_cases/publish_notification.py
@@ -2,9 +2,11 @@ from app.domain.ports.event_bus import EventBus
 from app.domain.exceptions.notification_publish_error import NotificationPublishError
 from app.domain.entities.notification_request import NotificationRequest
 from app.domain.events.notification_event import NotificationEvent
+from app.domain.repositories.user_contact_info_repository import UserContactInfoRepository
+from app.domain.exceptions.user_not_found_exception import UserNotFound
 import logging
 
-logging.basicConfig(level=logging.INFO)
+
 logger = logging.getLogger(__name__)
 
 class PublishNotificationUseCase:
@@ -12,27 +14,40 @@ class PublishNotificationUseCase:
     def __init__(self,event_bus:EventBus):
         self.event_bus = event_bus
 
-    def execute(self,notification:NotificationRequest,user_repo) -> None:
+    def execute(self,notification:NotificationRequest,
+                user_contact_info_repository: UserContactInfoRepository) -> None:
         logger.info(f"Trying to publish notification ...")
-        
-        if notification.channel == "email":
-            destination = user_repo.email
 
-        elif notification.channel == "sms":
-            destination = user_repo.sms
+        try:
 
-        elif notification.channel == "push":
-            destination = user_repo.push
+            user_contact = user_contact_info_repository.get_contactc_info(notification.user_id)
 
-        
-        event = NotificationEvent(notification.user_id,
-                                  notification.message,
-                                  notification.channel,
-                                  destination)
+            if not user_contact:
+                raise UserNotFound("[PUBLISH SERVICE] User not found")
+            
+            if notification.channel == "email":
+                destination = user_contact.email
 
-        success = self.event_bus.publish(event)
-        if not success:
-            logger.error(f" Failes dto publish notification")
-            raise NotificationPublishError("Failed to publish event")
-        
-        logger.info("Notication published with successful.")
+            elif notification.channel == "sms":
+                destination = user_contact.sms
+
+            elif notification.channel == "push":
+                destination = user_contact.push
+
+            
+            event = NotificationEvent(notification.user_id,
+                                    notification.message,
+                                    notification.channel,
+                                    destination)
+
+            success = self.event_bus.publish(event)
+            if not success:
+                logger.error(f" Failes dto publish notification")
+                raise NotificationPublishError("Failed to publish event")
+         
+            logger.info("Notication published with successful.")
+        except UserNotFound:
+            raise
+        except Exception as e:
+            logger.exception(f"[PUBLISH SERVICE] Unexpected error while publish event:{e}")
+            raise
