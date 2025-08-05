c7732bac8a4df052a268380f77d1546fe5518e60
karllabatista <kblb@icomp.ufam.edu.br>
2025-08-04 23:01:38 -0400

    implements api router

--- user-service/app/main.py
+++ user-service/app/main.py
@@ -1,34 +1,9 @@
-from fastapi import FastAPI,Path,Depends,HTTPException
-from app.use_cases.get_contact_info_use_case import GetContactInfoUseCase
-from app.infrastructure.in_memory_repository import InMemoryRepository
-from app.domain.exception.user_not_found_exception import UserNotFoundException
-from app.interface.schemas.contact_info_output import ContactInfoOutput
-from starlette import status
+from fastapi import FastAPI
+from app.interface.controllers.get_user_contact_info_controller import router
 import logging
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 app = FastAPI()
-
-def config_use_case()-> GetContactInfoUseCase:
-    repository = InMemoryRepository()
-    return GetContactInfoUseCase(repository)
-
-@app.get("/users/{user_id}/contact-info",status_code=status.HTTP_200_OK,response_model=ContactInfoOutput)
-def get_user_contact_info(
-    user_id: str = Path(..., min_length=4, max_length=10),
-    get_user_contact_info_use_case:GetContactInfoUseCase = Depends(config_use_case)
-) :
-    try:
-      
-        contact = get_user_contact_info_use_case.execute(user_id)
-        
-        return ContactInfoOutput.from_entity(contact)
-
-    except UserNotFoundException:
-        logger.warning(f"User not found: {user_id}")
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail={"message": "user not found"})
-    except Exception as e:
-        logger.error(f"Unexpected error while fetching user {user_id}: {e}")
-        raise HTTPException( status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,detail="Internal server error")
+app.include_router(router)
