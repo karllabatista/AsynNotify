dba55b6d8f4cf3591820192c7c6d2e4ca73171d8
karllabatista <kblb@icomp.ufam.edu.br>
2025-08-06 18:43:40 -0400

    fix: fix error in logging configuration

--- notification-publish-service/app/main.py
+++ notification-publish-service/app/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from app.interface.controllers.notification_controller import router
 import logging
 
-logging.basicConfig(level=logging.info)
+logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 app = FastAPI()
