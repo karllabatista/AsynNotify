077ab4703680c3676bace8486cf19997c1657d80
karllabatista <kblb@icomp.ufam.edu.br>
2025-08-06 22:52:14 -0400

    test: add test to notification class

---
+++ notification-dispatch-service/tests/unit/domain/entities/test_notification_class.py
@@ -0,0 +1,29 @@
+from app.domain.entitites.notification import Notification
+import pytest
+def test_create_notification_instance():
+    
+    # ARRANGE
+    user_id ="test_user_0123"
+    message = " This is a message test"
+    channel = " email"
+    destination =  "test@email.com"
+
+
+    # ACT
+    notification = Notification(user_id,message,channel,destination)
+
+    # ASSERT
+
+    assert isinstance(notification,Notification)
+
+def test_create_notification_invalid_fields_raises_value_error():
+
+    # ARRANGE
+    message = " This is a message test"
+    channel = " email"
+    destination =  "test@email.com"
+
+    # ACT+ASSERT
+
+    with pytest.raises(ValueError,match="Invalid mandatory fields"):
+        Notification("",message,channel,destination)
