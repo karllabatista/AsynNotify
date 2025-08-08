e24b059bfa6e9bddbff08856d2d5110ce2261bf5
karllabatista <kblb@icomp.ufam.edu.br>
2025-08-08 00:21:36 -0400

    chore: create a event consumer service

---
+++ notification-dispatch-service/app/application/services/event_consumer_service.py
@@ -0,0 +1,25 @@
+from domain.ports.event_bus.event_bus import EventBus
+from domain.exceptions.event_bus_error_exception import EventBusErrorException
+import logging
+logger = logging.getLogger(__name__)
+class EventConsumerService:
+
+    def __init__(self,
+                event_bus:EventBus):
+        
+        self.event_bus = event_bus
+
+    def consumer(self)->dict:
+        try:
+
+            logger.info("[DISPATCH SERVICE] Get event in notification queue ..")
+            event = self.event_bus.consumer()
+            return event
+        
+        except EventBusErrorException as event_error:
+            logger.error(f"Failed consumer event: {event_error}")
+            raise
+
+
+    
+    
