0a04b86106c43ec2b09ca8d8ddc95566158042a8
karllabatista <kblb@icomp.ufam.edu.br>
2025-08-07 23:14:10 -0400

    chore: isolate  tasks: consumer,parse event and create notification

--- notification-dispatch-service/app/use_cases/dispatch_notification.py
+++ notification-dispatch-service/app/use_cases/dispatch_notification.py
@@ -3,6 +3,7 @@ from domain.ports.dispatchers.channel_dispatcher import ChannelDispatcher
 from domain.entitites.notification import Notification
 from domain.exceptions.event_bus_error_exception import EventBusErrorException
 from domain.exceptions.channel_dispatcher_error_exception import ChannelDispatchErrorException
+from domain.exceptions.invalid_notification_event import InvalidNotificationEvent
 import logging
 
 logger = logging.getLogger(__name__)
@@ -13,37 +14,47 @@ class DispatchNotificationUseCase:
         self.event_bus = event_bus
         self.channel_dispatcher= channel_dispatcher
 
-    def execute(self)-> None:
-        logger.info("[DISPATCH SERVICE] Try to process event")
+
+    def execute(self):
+        
+        notification = self.create_notification_from_event()
+        try:
+            self.channel_dispatcher.dispatch(notification)
+
+        except ChannelDispatchErrorException as channel_error:
+            logger.error(f"Failed dispatcher notification: {channel_error}")
+            raise   
+
+    def create_notification_from_event(self)-> Notification:
+
+        event = self._event_consumer()
+
+        return self._parse_event_to_notification(event)
+
+
+    def  _event_consumer(self)->dict:
 
         try:
-        
-            # get event of queue
+
             logger.info("[DISPATCH SERVICE] Get event in notification queue ..")
             event = self.event_bus.consumer()
+            return event
+        
         except EventBusErrorException as event_error:
             logger.error(f"Failed consumer event: {event_error}")
             raise
+    
 
-        # parse event to notification
-        logger.info("[DISPATCH SERVICE] Parsing event to notification ..")
-        notification = self._parse_event_to_notification(event)
-        logger.info("[DISPATCH SERVICE] is ready to dispatcher")
-        try:
-            # redirect notification to specific channel
-            self.channel_dispatcher(notification)
-        except ChannelDispatchErrorException as channel_error:
-            logger.error(f"Failed dispatcher notification: {channel_error}")
-            raise    
+    def _parse_event_to_notification(self,event:dict)-> Notification: 
 
-    def _parse_event_to_notification(self,event:dict)-> Notification:
+        data = event.get("data")
 
-        user_id = event.get("data", None).get("user_id",None)
-        message = event.get("data", None).get("message",None)
-        channel = event.get("data", None).get("channel",None)
-        destination = event.get("data", None).get("destination",None)
+        if not data:
+            logger.error("Missing data key in event")
+            raise InvalidNotificationEvent("Missing data key in event")
+
         
-        return Notification(user_id=user_id,
-                            message=message,
-                            channel=channel,
-                            destination=destination)
+        return Notification(user_id=data.get("user_id"),
+                            message=data.get("message"),
+                            channel=data.get("channel"),
+                            destination=data.get("destination"))
