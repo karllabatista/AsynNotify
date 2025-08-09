6c5dad540f6903d275cc0b0eb441f202981abf63
karllabatista <kblb@icomp.ufam.edu.br>
2025-08-09 14:13:26 -0400

    test: change content of event dict

--- notification-dispatch-service/tests/integration/infrastructure/ports/event_bus/test_redis_event_bus.py
+++ notification-dispatch-service/tests/integration/infrastructure/ports/event_bus/test_redis_event_bus.py
@@ -22,7 +22,19 @@ def test_redis_event_bus_consumer_event(redis_client):
     timeout = 5
     queue = "notifications_test"
 
-    event = {"type":"test","payload":"ok"}
+    event = {
+    "event_type": "notification_sent",
+    "data": {
+        "user_id": "123",
+        "message": "Hello World",
+        "channel": "email",
+        "destination": "user@example.com"
+    },
+    "metadata": {
+        "timestamp": "2025-08-09T13:00:00Z",
+        "request_id": "012030"
+    }
+}
     redis_client.lpush(queue,json.dumps(event))
 
     #ACT
@@ -33,9 +45,11 @@ def test_redis_event_bus_consumer_event(redis_client):
 
     # ASSERT
     assert isinstance(result,dict)
-    assert "type" in result
-    assert "payload" in result
-    assert result["type"] == "test"
-    assert result["payload"] == "ok"
+    assert "data" in result
+    assert "metadata" in result
+    assert result["data"]["user_id"] == "123"
+    assert result["data"]["message"] == "Hello World"
+    assert result["data"]["channel"] == "email"
+    assert result["data"]["destination"] ==  "user@example.com"
 
 
