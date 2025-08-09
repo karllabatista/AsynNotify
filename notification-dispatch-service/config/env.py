babf278a38da404137e06beea38fad45eb50a3c4
karllabatista <kblb@icomp.ufam.edu.br>
2025-08-09 12:40:53 -0400

    chore:  add function to redis connection

--- notification-dispatch-service/config/env.py
+++ notification-dispatch-service/config/env.py
@@ -6,3 +6,12 @@ load_dotenv()
 def get_queue():
 
     return os.getenv("QUEUE_NAME","notifications")
+
+def get_redis_host():
+    return os.getenv("REDIS_HOST", "localhost")
+
+def get_redis_port():
+    return int(os.getenv("REDIS_PORT", 6379))
+
+def get_value_of_timeout_queue():
+    return int(os.getenv("TIMEOUT","10"))
