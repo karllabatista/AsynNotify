a4e52ea00804f8193edd2b1db650145a566359fb
karllabatista <kblb@icomp.ufam.edu.br>
2025-08-10 23:23:28 -0400

    feat: convert get redis connection to method async with aioredis

--- notification-dispatch-service/src/infrastructure/ports/redis_client.py
+++ notification-dispatch-service/src/infrastructure/ports/redis_client.py
@@ -1,10 +1,11 @@
-from redis import Redis
+import aioredis
 from config.env import get_redis_host, get_redis_port
 
-def get_redis_connection()-> Redis:
-    return Redis(
-        host=get_redis_host(),
-        port=get_redis_port(),
+async def get_redis_connection() -> aioredis.Redis:
+    redis_url = f"redis://{get_redis_host()}:{get_redis_port()}"
+    redis = aioredis.from_url(
+        redis_url,
         db=0,
         decode_responses=True
     )
+    return redis
