197155a0c9daf9a66590a70ae5b0c46fed1e50de
karllabatista <kblb@icomp.ufam.edu.br>
2025-08-11 16:33:22 -0400

    feat: encasulate creatio o f content of email

--- notification-dispatch-service/src/infrastructure/ports/dispatchers/email_channel_dispatch.py
+++ notification-dispatch-service/src/infrastructure/ports/dispatchers/email_channel_dispatch.py
@@ -1,28 +1,49 @@
 from src.domain.ports.dispatchers.channel_dispatcher  import ChannelDispatcher
 from src.domain.entitites.notification import Notification
+from src.domain.exceptions.email_channel_dispatch_error import EmailDispatcherException
+from src.domain.ports.services.email_service import EmailService
 import logging
 import asyncio
 logger = logging.getLogger(logging.INFO)
 
 class EmailChannelDispatch(ChannelDispatcher):
 
+    def __init__(self,service:EmailService):
+        self.service = service
+
 
     async def dispatch(self, notification:Notification) -> None:
         """
         Dispatch a notification to email channel
+        
         """
         try:
-            send_notification = {
-                "message": notification.message,
-                "destination":notification.destination
-            }
             
-            logger.info(f"Dispatching notification to {notification.channel} channel")
-            logger.debug(f"Payload: {send_notification}")
-            await asyncio.sleep(2) 
-            logger.info(f"Notifcation sended to {notification.destination}")
+            content = self._create_content_email(notification)
 
+
+            # logger.info(f"Dispatching notification to {notification.channel} channel")
+            # logger.debug(f"Payload: {email_notification}")
+            # await asyncio.sleep(2) 
+            # logger.info(f"Notifcation sent to {notification.destination}")
+            await self.service.send(content)
+
+        except EmailDispatcherException as email_error:
+            logger.error("[EMAILCHANNELDISPATCH] Failed to dispatch notification", exc_info=email_error)
+            raise
         except Exception as e:
-            logger.exception("[EMAICHANNELDISPATCH] An error occurred while send noticfication by email")
+            logger.exception("[EMAILCHANNELDISPATCH] An internal error occurred while dispatch noticfication by email")
 
             raise
+    
+    def _create_content_email(self,notification:Notification)->dict:
+        if not notification.message or not notification.destination:
+            raise ValueError("Notification must have message and destination")
+
+        content = {
+                "message": notification.message,
+                "destination":notification.destination
+                }
+        return content
+
+        
