988ea59116dec652c78a109885e5b4f9c26f9807
karllabatista <kblb@icomp.ufam.edu.br>
2025-08-12 11:35:41 -0400

    feat: add router channel class

---
+++ notification-dispatch-service/src/infrastructure/ports/dispatchers/channel_dispatch_router.py
@@ -0,0 +1,24 @@
+from typing import Dict
+from src.domain.ports.dispatchers.channel_dispatcher import ChannelDispatcher
+from src.domain.entitites.notification import Notification
+from src.domain.exceptions.channel_dispatcher_error_exception import ChannelDispatchErrorException
+class ChannelDispatchRouter(ChannelDispatcher):
+
+    def __init__(self,dispatchers:Dict[str,ChannelDispatcher]):
+
+        self.dispatchers = dispatchers
+
+    async def dispatch(self,notification:Notification)->None:
+        """
+        Dispatch notification for a specific channel
+        """
+       
+       
+        dispatch =  self.dispatchers.get(notification.channel)
+
+        if not dispatch:
+            raise ChannelDispatchErrorException(f"Channel does not supported:{notification.channel}")
+
+        await dispatch.dispatch(notification)
+
+
