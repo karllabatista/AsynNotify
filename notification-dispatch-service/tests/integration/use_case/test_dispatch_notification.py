48f8d255be1e0c8243995b19dd5527d0a1bc2b64
karllabatista <kblb@icomp.ufam.edu.br>
2025-08-12 11:50:44 -0400

    test: add failure when data key is missing

--- notification-dispatch-service/tests/integration/use_case/test_dispatch_notification.py
+++ notification-dispatch-service/tests/integration/use_case/test_dispatch_notification.py
@@ -8,6 +8,7 @@ from src.application.services.notification_factory import NotificationFactory
 from src.infrastructure.ports.dispatchers.channel_dispatch_router import ChannelDispatchRouter
 from src.infrastructure.ports.dispatchers.email_channel_dispatch import EmailChannelDispatch
 from src.infrastructure.ports.services.faker_email_service import FakerEmailService
+from src.domain.exceptions.invalid_notification_event import InvalidNotificationEvent
 
 
 QUEUE_NAME = "notificationd-test"
@@ -65,4 +66,42 @@ async def test_dispatch_notification_success(redis_client):
 
     # ASSERT - garante que o envio foi chamado uma vez
     assert service.last_email["destination"] == "test@test.com"
+
+@pytest.mark.asyncio
+
+async def test_dispatch_notification_raises_data_key_missing(redis_client):
+    # ARRANJE
+    # insert a new event in the queue
     
+    event ={
+            "event_type": "NotificationRequested",
+            "metadata":{
+                "timestamp":"",
+                "request_id": "ABCB-request"
+            }
+    }
+    await redis_client.lpush(QUEUE_NAME,json.dumps(event))
+
+    consumer = RedisEventBus(redis_client,QUEUE_NAME,timeout=5)
+  
+    # call notification object factory
+    notification_factory = NotificationFactory()
+
+    service = FakerEmailService()
+
+    dispatchers = {
+        "email":EmailChannelDispatch(service)
+        
+    }
+    channel_dispatch = ChannelDispatchRouter(dispatchers)
+
+    # ACT + ASSERT
+
+    with pytest.raises(InvalidNotificationEvent) as  excinfo:
+        dispatch_use_case = DispatchNotificationUseCase(consumer,
+                                                    notification_factory,
+                                                    channel_dispatch)
+        # just checks if it doesn't throw an exception  
+        await dispatch_use_case.execute()
+
+    assert "Missing data key in event" in str(excinfo.value)    
