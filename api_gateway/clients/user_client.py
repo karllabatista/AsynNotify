ba30e4bfd6a8809d7573e91a97944e8eea208823
karllabatista <kblb@icomp.ufam.edu.br>
2025-08-13 16:49:01 -0400

    feat: Created user service clients to encapsulate HTTP calls to internal services

---
+++ api_gateway/clients/user_client.py
@@ -0,0 +1,14 @@
+import httpx
+class UserClient:
+
+    def __init__(self,base_url):
+        
+        self.base_url =base_url
+
+    
+    async def get_user_contact_info(self,user_id:str):
+        
+        async with httpx.AsyncClient() as client:
+            resp = await client.get(f"{self.base_url}/users/{user_id}/contact-info")
+
+        return resp.json()              
