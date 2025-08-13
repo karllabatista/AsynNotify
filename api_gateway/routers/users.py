f3d97ddf66961e7ec6fa98ac9fd65eefff1136b1
karllabatista <kblb@icomp.ufam.edu.br>
2025-08-13 16:49:56 -0400

    feat: call to user service client  to call http asyn calls

--- api_gateway/routers/users.py
+++ api_gateway/routers/users.py
@@ -1,12 +1,12 @@
 from fastapi import APIRouter
 import httpx
+from clients.user_client import UserClient
 
+base_url = "http://127.0.0.1:8001/"
 router = APIRouter()
+user_client = UserClient(base_url)
 
 @router.get("/users/{user_id}/contact-info")
 async def get_user_contact_info(user_id:str):
-    
-    async with httpx.AsyncClient() as client:
-        resp = await client.get(f"http://127.0.0.1:8001/users/{user_id}/contact-info")
-
-    return resp.json()              
+ 
+    return await user_client.get_user_contact_info(user_id)
