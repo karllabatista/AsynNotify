26e80918d74f345b71d0d9f60b8cbc0367ab35b4
karllabatista <kblb@icomp.ufam.edu.br>
2025-08-13 20:29:23 -0400

    refactor: add base url and call to notification client

--- api_gateway/routers/notification.py
+++ api_gateway/routers/notification.py
@@ -1,19 +1,18 @@
 from fastapi import APIRouter,Request
-import httpx
+from clients.notification import Notification
 
 router = APIRouter()
 
+base_url ="http://127.0.0.1:8002" 
+notification_client = Notification(base_url)
+
 @router.post("/notifications")
-async def  publish_notification(req:Request):
+async def  publish_notification_endpoint(req:Request):
     """
     curl -X POST -d '{"user_id": "karlla","channel": "email","message": "Você tem uma nova mensagem."}'  http://127.0.0.1:8000/notifications
     
     """
-    
     payload = await req.json()
-    
-    async with httpx.AsyncClient() as client:
+        
+    return await notification_client.publish_notification(payload)
 
-        result = await client.post(f"http://127.0.0.1:8002/notifications",json=payload) 
-    
-    return result.json()
