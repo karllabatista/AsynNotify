b78d5aaa8ecedc83dec6b33335756358ceaae9b0
karllabatista <kblb@icomp.ufam.edu.br>
2025-08-13 20:25:37 -0400

    fix: rename async function  ad remove bar of endpoint

--- api_gateway/routers/users.py
+++ api_gateway/routers/users.py
@@ -1,12 +1,11 @@
 from fastapi import APIRouter
-import httpx
 from clients.user_client import UserClient
 
-base_url = "http://127.0.0.1:8001/"
+base_url = "http://127.0.0.1:8001"
 router = APIRouter()
 user_client = UserClient(base_url)
 
 @router.get("/users/{user_id}/contact-info")
-async def get_user_contact_info(user_id:str):
+async def get_user_contact_info_endpoint(user_id:str):
  
     return await user_client.get_user_contact_info(user_id)
