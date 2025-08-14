755b98545341b3c49f1fe8f4f5a7147e3be55153
karllabatista <kblb@icomp.ufam.edu.br>
2025-08-13 20:46:31 -0400

    fix: call the right client http user

--- api_gateway/routers/users.py
+++ api_gateway/routers/users.py
@@ -1,9 +1,9 @@
 from fastapi import APIRouter
-from clients.user_client import UserClient
+from api_gateway.clients.user_client_htpp import UserClientClientHTTPAsync
 
 base_url = "http://127.0.0.1:8001"
 router = APIRouter()
-user_client = UserClient(base_url)
+user_client = UserClientClientHTTPAsync(base_url)
 
 @router.get("/users/{user_id}/contact-info")
 async def get_user_contact_info_endpoint(user_id:str):
