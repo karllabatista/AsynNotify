095e03f732bb4019dc214cf992231cc0a343898f
karllabatista <kblb@icomp.ufam.edu.br>
2025-08-17 15:25:21 -0400

    feat (notification-dispatch work) : create a SMSChannelDispatcher Signed-off-by: Karlla Batista <karllabatista19@gmail.com>

---
+++ notification-dispatch-service/src/infrastructure/ports/dispatchers/sms_channel_dispatcher.py
@@ -0,0 +1,62 @@
+from src.domain.ports.dispatchers.channel_dispatcher import ChannelDispatcher
+from src.domain.ports.services.sms_service import SMSService
+from src.application.services.notification_factory import NotificationFactory
+from src.domain.exceptions.channel_dispatcher_error_exception  import ChannelDispatchErrorException
+import logging
+
+logger = logging.getLogger(__name__)
+class SMSChannelDispatcher(ChannelDispatcher):
+    # TODO
+    # retries and fallback
+
+    def __init__(self, service:SMSService):
+        self.service = service
+
+
+    async def dispatch(self, notification:NotificationFactory):
+
+        """
+        Receive a generic notification,check them and send to Service
+        """
+        try:
+            logger.info(f"[SMS-Dispatcher] Start dispatcher notification to the {notification.channel}")
+    
+            if self._validate_notification(notification):
+                
+                logger.info (f"[SMS-Dispatcher] Validate mandatory fields OK")
+                payload = self._create_payload_sms(notification)
+                
+                logger.info("[SMS-Dispatcher] Payload created for SMS Service")
+                result =  await  self.service.send(payload)
+
+                logger.info(f"[SMS-Dispatcher] Notification dispatched successfully: {result}") 
+                return result
+            
+        except ChannelDispatchErrorException as e:
+            logger.error(f"[SMS-Dispatcher] Channel dispatch error: {e}")
+            raise
+
+        except Exception as e:
+            logger.exception(f"[SMS-Dispatcher] Unexpected error: {e}")
+            raise
+    
+    def _validate_notification(self, notification:NotificationFactory): 
+        """
+        Validates if mandatory fields are presents
+        """
+        logger.debug("[SMS-Dispatcher] Validating mandatory fields...")
+        if not notification.message or not notification.destination:
+            raise ValueError(" Missing mandatory fields")
+        
+        return True
+    
+    def _create_payload_sms(self,notification:NotificationFactory):
+        """
+        Converts the genreci notification to channel format
+        """
+        logger.debug("[SMS-Dispatcher] Converting notification to SMS payload")
+        return {
+            "message": notification.message,
+            "destination": notification.destination  
+
+        }
