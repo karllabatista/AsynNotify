136bb10a941eb0df8c93cb6828fb694c9685386d
karllabatista <kblb@icomp.ufam.edu.br>
2025-08-17 20:25:49 -0400

    feat (notification-dispatch-work): change logic implementation of send to provider method Signed-off-by: Karlla Batista <karllabatista19@gmail.com>

--- notification-dispatch-service/src/infrastructure/ports/services/faker_email_service.py
+++ notification-dispatch-service/src/infrastructure/ports/services/faker_email_service.py
@@ -2,17 +2,13 @@ from src.domain.ports.services.email_service import EmailService
 from src.domain.exceptions.email_service_exception import EmailServiceException
 import logging
 import asyncio
-from email.message import EmailMessage
-from typing import Dict
+import random
 
 logger = logging.getLogger(__name__)
 class FakerEmailService(EmailService):
 
-    def __init__(self):
-        self.last_email = ""
-
-
-    async def send(self, content:dict):
+ 
+    async def send_to_provider(self, content:dict) -> dict:
 
         
         """
@@ -26,34 +22,36 @@ class FakerEmailService(EmailService):
             ValueError: If mandatory keys are missing in content.
             EmailServiceException: For domain specific email errors.
         """
-        
-        try:
-            self._validate_input(content)
-            # guarantee send stateless and independent
-            msg = EmailMessage()
-            msg["Subject"] = content.get('subject','No subject')
-            msg["From"] = 'study@studycloud.com.br'
-            msg["To"] = content.get('destination','unknown@example.com')
+        logger.debug("Trying to send email through provider..")
+        await asyncio.sleep(0.3)
+
+        chance = random.random()
+
+        if chance < 0.7: # [0.0,0.7]: success
+            logger.debug("[FakeEmailService]Email sent with success")
+            return {
+                "status": "sent",
+                "message_id": f"msg_{random.randint(1000,9999)}",
+                "provider": "FakeEmail",
+                "to": content["to"]
+            }
+        elif chance <0.85: # [0.7,0.85] # temporary error
+            logger.error("[FakeEmailService]Email sending failed:Mailbox busy, retry later")
+            return {
+                "status": "failed",
+                "error": "temporary_error",
+                "message": "Mailbox busy, retry later"
+            }
+        elif chance <0.95: # [0.85,0.95]permanent error
+            logger.error("[FakeEmailService]Email sending failed:Email address does not exist")
+            return {
+                "status": "failed",
+                "error": "invalid_recipient",
+                "message": "Email address does not exist"
+            }
+        else:  
+            logger.error("[FakeEmailService] Email sending failed:Email provider did not respond in time")
+            raise TimeoutError("Email provider did not respond in time")
 
 
-            body = content.get('message') or ''
-            msg.set_content(body)
 
-            logger.info(f"[FAKE EMAIL SERVICE SENT] EMAIL to: {msg['To']}")
-            self.last_email = {"destination":msg["To"]}
-
-            logger.debug(f"Subject: {msg['Subject']}")
-            logger.debug(f"Email content:\n{msg.as_string()}")
-
-            await asyncio.sleep(0.5)
-        except EmailServiceException as e:
-            logger.error(f"[FAKE EMAIL SERVICE ERROR] Failed to send email:{e}")
-            raise
-        except Exception as e:
-            logger.exception(f"[FAKE EMAIL SERVICE ERROR] An error occorred when send email:{e}")
-            raise
-
-    def _validate_input(self,content:Dict[str,str]) -> None:
-
-        if not content.get('message') or not content.get('destination'):
-            raise ValueError("Missing mandatory keys 'message' or 'destination'")
