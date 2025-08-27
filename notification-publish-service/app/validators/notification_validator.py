14d49363f5fe9725239a76ff3332f8dc7b6b9310
karllabatista <kblb@icomp.ufam.edu.br>
2025-08-26 23:29:17 -0400

    feat: add validotr to notification

---
+++ notification-publish-service/app/validators/notification_validator.py
@@ -0,0 +1,11 @@
+class NotificationValidator:
+    VALID_CHANNELS = {"email", "sms", "push"}
+
+    @staticmethod
+    def validate(user_id:str,message:str,channel:str) ->bool:
+      
+        if not user_id or not message:
+            return False
+        if channel not in VALID_CHANNELS:
+            return False
+        return True
