cebb7e733a6f9b036b15963714b86bbf96756945
karllabatista <kblb@icomp.ufam.edu.br>
2025-08-29 16:20:13 -0400

    build:  add topic env

--- notification-publish-service/config/env.py
+++ notification-publish-service/config/env.py
@@ -16,3 +16,6 @@ def get_queue_name():
 def get_base_url_user_service():
 
     return os.getenv("BASE_URL_USER_SERVICE","http://127.0.0.1:8000")
+
+def get_kafka_topic_name():
+    return os.getenv("TOPIC","notifications")
