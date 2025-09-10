0279d100a4803dd439d043f200d612ffc12f55ca
karllabatista <kblb@icomp.ufam.edu.br>
2025-09-10 15:15:38 -0400

    feat:  improve use case: - change input of execution function - refactor input of  user_repository.get_contact_info_by_user_id - refactor input notification factoriy instance -  implements eventual consistence - refactor success log

--- notification-publish-service/app/use_cases/publish_notification.py
+++ notification-publish-service/app/use_cases/publish_notification.py
@@ -4,6 +4,7 @@ from app.domain.events.notification_event import NotificationEvent
 from app.domain.repositories.user_contact_info_repository import UserContactInfoRepository
 from app.domain.exceptions.user_not_found_exception import UserNotFound
 from app.factory.notification_factory import NotificationFactory
+from app.interface.schemas.notification_input import NotificationInput
 import logging
 
 
@@ -17,12 +18,12 @@ class PublishNotificationUseCase:
         self.event_bus = event_bus
         self.user_repository = user_repository
 
-    def execute(self,notification:dict) -> None:
+    def execute(self,notification_input:NotificationInput) -> None:
         logger.info(f"Trying to publish notification ...")
 
         try:
             # TODO push notification
-            user_contact =  self.user_repository.get_contact_info_by_user_id(notification["user_id"])
+            user_contact =  self.user_repository.get_contact_info_by_user_id(notification_input.user_id)
 
             if not user_contact:
                 raise UserNotFound("[PUBLISH SERVICE] User not found")
@@ -35,9 +36,9 @@ class PublishNotificationUseCase:
             raise   
 
         # create a notification with facotry
-        notification = NotificationFactory.create(user_id=notification["user_id"],
-                                                  message=notification["message"],
-                                                  channel=notification["channel"])
+        notification = NotificationFactory.create(user_id=notification_input.user_id,
+                                                  message=notification_input.message,
+                                                  channel=notification_input.channel)
 
 
         # notification entity becomes event
@@ -50,24 +51,10 @@ class PublishNotificationUseCase:
                                 )
         
         try:
-            # TODO Decide o que fazer se falhar:
-
-            # Retry com backoff
-
-            # Jogar em uma DLQ
-
-            # Registrar uma métrica de falha
-
-            # Retentar em outro broker (failover)
-            success = self.event_bus.publish(event)
-            if not success:
-
-                logger.error(f" Failed to publish notification")
-                raise NotificationPublishError("Failed to publish event")
+            self.event_bus.publish(event)
          
-            logger.info("Notication published with successful.")
-        except NotificationPublishError:
-            raise
+            logger.info("[PUBLISH SERVICE] Notication published with successful.")
+
         except Exception as e:
             logger.exception(f"[PUBLISH SERVICE] Unexpected error while publish event:{e}")
             raise NotificationPublishError("Failed to publish event") from e
