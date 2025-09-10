c645fe0b71b0c4d2fa4cbfa3ccab311f31d1541b
karllabatista <kblb@icomp.ufam.edu.br>
2025-09-09 22:09:09 -0400

    test: add tests to deliverry report callback function

--- notification-publish-service/tests/unit_tests/infrastcuture/messaging/test_kafka_event_bus.py
+++ notification-publish-service/tests/unit_tests/infrastcuture/messaging/test_kafka_event_bus.py
@@ -3,7 +3,7 @@ from app.infrastructure.messaging.kafka_event_bus import KafkaEventBus,TOPIC
 import pytest
 from unittest.mock import MagicMock
 import json
-
+import logging
 @pytest.fixture
 def event():
     return NotificationEvent(
@@ -40,3 +40,32 @@ def test_publish_notification_in_broker_kafka_success(event,event_bus):
     )
 
     event_bus.producer.flush.assert_called_once_with(timeout=5)
+
+
+def test_delivery_report_success(caplog,event_bus):
+    
+
+    # caplog to capture and logging output from a test
+
+    caplog.set_level(logging.INFO)
+    msg = MagicMock()
+    event_bus._delivery_report(err=None,msg=msg)
+
+    assert "Message produced:" in caplog.text
+    assert str(msg) in caplog.text
+
+    
+def test_delivery_report_failure(caplog,event_bus):
+    
+
+    # caplog to capture and logging output from a test
+
+    caplog.set_level(logging.INFO)
+    msg = MagicMock()
+
+    event_bus._delivery_report(err="some-error",msg=msg)
+
+    assert "Failed to deliver message" in caplog.text
+    assert str(msg) in caplog.text
+
+
