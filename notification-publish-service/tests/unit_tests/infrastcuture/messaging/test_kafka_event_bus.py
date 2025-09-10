d76666cfca493ae025e5a6290ebd24a304e12073
karllabatista <kblb@icomp.ufam.edu.br>
2025-09-10 12:05:49 -0400

    tests: add tests to delivery report funciton

--- notification-publish-service/tests/unit_tests/infrastcuture/messaging/test_kafka_event_bus.py
+++ notification-publish-service/tests/unit_tests/infrastcuture/messaging/test_kafka_event_bus.py
@@ -69,3 +69,38 @@ def test_delivery_report_failure(caplog,event_bus):
     assert str(msg) in caplog.text
 
 
+def test_publish_event_with_success_delivery_report(event,event_bus,caplog):
+    # arrange
+    caplog.set_level(logging.INFO)
+
+    def fake_produce(TOPIC,key,value,callback):
+        callback(err=None,msg="fake-msg")
+
+    event_bus.producer.produce.side_effect = fake_produce
+
+    
+    # act
+    event_bus.publish(event)    
+    # assert
+
+    assert "Message produced:" in caplog.text
+    event_bus.producer.flush.assert_called_once_with(timeout=5)
+
+
+def test_publish_event_with_failed_delivery_report(event,event_bus,caplog):
+    # arrange
+    caplog.set_level(logging.ERROR)
+
+    def fake_produce(TOPIC,key,value,callback):
+        callback(err="some-error",msg="fake-mg")
+
+    event_bus.producer.produce.side_effect = fake_produce
+
+    
+    # act
+    event_bus.publish(event)    
+    # assert
+
+    assert "Failed to deliver message" in caplog.text
+    assert "some-error" in caplog.text
+    event_bus.producer.flush.assert_called_once_with(timeout=5)
